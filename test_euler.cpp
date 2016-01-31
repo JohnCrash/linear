872d5457aeb18773f93457c4cc1d9ff3ec819d34
johncrash <udsn2@sina.com>
2016-01-31 17:55:56 +0800

    done

--- test_euler.cpp
+++ test_euler.cpp
@@ -7,9 +7,13 @@ real func_0(real t,real x)
 	return x+1;
 }
 
-real func_0_solve(real t)
+//求参数,b=false求参数
+real func_0_solve(real t,real A,bool b)
 {
-	return pow(M_E,t)-1;
+	if(b)
+		return A*pow(M_E,t)-1;
+	else
+		return (A+1)/pow(M_E,t);
 }
 
 /*
@@ -36,17 +40,18 @@ real eulerPrint(real (*f)(real,real),real t0,real x0,real step,int n)
 	}	
 }
 
-real solvePrint(real (*f)(real),real t0,real x0,real step,int n)
+real solvePrint(real (*f)(real,real,bool),real t0,real x0,real step,int n)
 {
-	real t,x,h;
+	real t,x,h,A;
 	t = t0;
 	x = x0;
 	h = step;
+	A = f(t,x,false);
 	printf("formula solve,initial value t=%.4f x=%.4f step=%.4f\n",t0,x0,step);
 	printf("%.4f\t|%.4f\n",t,x);	
 	for(int i=0;i<n;i++){
-		x = f(t);
 		t+=h;
+		x = f(t,A,true);
 		printf("%.4f\t|%.4f\n",t,x);
 	}	
 }
@@ -54,8 +59,22 @@ real solvePrint(real (*f)(real),real t0,real x0,real step,int n)
 //x'=1+x , x(0)=1
 void test_1()
 {
+	printf("solve x'=1+x\n");
 	eulerPrint(func_0,0,1,0.1,10);
+	printf("x=2*exp(t)-1\n");
 	solvePrint(func_0_solve,0,1,0.1,10);
+}
+
+void test_1()
+{
+	printf("solve x'=1+x\n");
+	eulerPrint(func_0,0,1,0.1,10);
+	printf("x=2*exp(t)-1\n");
+	solvePrint(func_0_solve,0,1,0.1,10);
+}
+
+void test_2()
+{
 }
 
 int main(int argn,char *argv[])
