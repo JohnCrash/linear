3ce84ec1cb606a8b085d6f111ffcdadff6a03751
johncrash <udsn2@sina.com>
2016-02-01 12:04:17 +0800

    中点法实现

--- readme.md
+++ readme.md
@@ -1,6 +1,15 @@
+##目的
+用来熟悉一些数学算法，所有的代码都没有进行优化，以便于更好的理解算法本身。
 ##实现一些代数计算
 ###LU分解
+考虑到数值精确性，每次将列中绝对值最大的作为主元。
 ###crout LU方法
+crout方法比标准的LU分解要快一倍左右。
 ###Cholesky分解
+(算法来源)[http://mathfaculty.fullerton.edu/mathews/n2003/CholeskyMod.html]
+(参考)[http://www.netlib.org/utk/papers/factor/node9.html]
 ###解逆矩阵
 ###解线性方程Ax=b
+##微分方程的数值解
+###欧拉法数值解
+###中点法数值解
--- test_euler.cpp
+++ test_euler.cpp
@@ -2,12 +2,19 @@
 #include <stdio.h>
 #include <math.h>
 
+/*
+ * 形如：x'=1+1的ODE
+ */
 real func_0(real t,real x)
 {
 	return x+1;
 }
 
-//求参数,b=false求参数
+/*
+ * x'=1+1的公式解，A是系数，x'=1+x时A=2
+ * 但是其实可以根据初始条件计算出A
+ * b=false求A，这时候t和A给出初值，b=true做正常函数计算
+ */
 real func_0_solve(real t,real A,bool b)
 {
 	if(b)
@@ -18,11 +25,22 @@ real func_0_solve(real t,real A,bool b)
 
 /*
  * Euler method solve difference equation
+ * 使用起点的斜率做计算
  */
 real eulerStep(real (*f)(real,real),real t0,real x0,real step)
 {
 	//x0+f(t0)* h
 	return x0+f(t0,x0)*step;
+}
+
+/*
+ * 终点点法使用step两端的斜率的平均值作为斜率进行计算
+ * 起点斜率是f(t0,x0)，终点斜率是f(t0+step,x0+f(t0,x0)*step)
+ * 终点是通过euler法进行的近似(t0+step,x0+f(t0,x0)*step)
+ */
+real midpointStep(real (*f)(real,real),real t0,real x0,real step)
+{
+	return x0+(f(t0,x0)+f(t0+step,x0+f(t0,x0)*step))*step/2;
 }
 
 real eulerPrint(real (*f)(real,real),real t0,real x0,real step,int n)
@@ -56,6 +74,21 @@ real solvePrint(real (*f)(real,real,bool),real t0,real x0,real step,int n)
 	}	
 }
 
+real midpointPrint(real (*f)(real,real),real t0,real x0,real step,int n)
+{
+	real t,x,h;
+	t = t0;
+	x = x0;
+	h = step;
+	printf("midpoint method,initial value t=%.4f x=%.4f step=%.4f\n",t0,x0,step);
+	printf("%.4f\t|%.4f\n",t,x);	
+	for(int i=0;i<n;i++){
+		x=midpointStep(func_0,t,x,h);
+		t+=h;
+		printf("%.4f\t|%.4f\n",t,x);
+	}		
+}
+
 //x'=1+x , x(0)=1
 void test_1()
 {
@@ -63,14 +96,8 @@ void test_1()
 	eulerPrint(func_0,0,1,0.1,10);
 	printf("x=2*exp(t)-1\n");
 	solvePrint(func_0_solve,0,1,0.1,10);
-}
-
-void test_1()
-{
-	printf("solve x'=1+x\n");
-	eulerPrint(func_0,0,1,0.1,10);
-	printf("x=2*exp(t)-1\n");
-	solvePrint(func_0_solve,0,1,0.1,10);
+	printf("midpoint\n");
+	midpointPrint(func_0,0,1,0.1,10);	
 }
 
 void test_2()
